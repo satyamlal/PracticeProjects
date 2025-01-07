3444c38d092d6084cac86434c5b2c385374d8b3e
Satyam Lal <satyamlal@users.noreply.github.com>
2025-01-07 20:26:08 +0530

    Fixing the function that starts the Game

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 //Logic of the game goes here
 let gameSeq = [];
 let userSeq = [];
+
+let btns = ["pink", "orange", "cyan", "blue"];
+
 let h2 = document.querySelector("h2");
 
 let started = false;
 let level = 0;
 
 document.addEventListener("keypress", function () {
-  if (started == false) {
+  if (!started) {
     console.log("Game Started!");
-    h2.innerHTML = "Game Started!!!";
     started = true;
   }
 });
+
+function btnFlash(btn) {
+  btn.classList.add("flash");
+  setTimeout(() => {
+    btn.classList.remove("flash");
+  }, 1000);
+}
+
+function levelUp() {
+  level++;
+  h2.textContent = `Level ${level}`;
+
+  const randIdx = Math.floor(Math.random() * btns.length);
+  const randColor = btns[randIdx];
+  const randBtn = document.querySelector(`.${randColor}`);
+
+  gameSeq.push(randColor);
+  console.log(randIdx);
+  console.log(randColor);
+  console.log(randBtn);
+
+  btnFlash(randBtn);
+}

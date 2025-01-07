4e9b9ce3127e537645ba950cf45f315c8e802a57
Satyam Lal <satyamlal@users.noreply.github.com>
2025-01-07 15:53:50 +0530

    Game Start Function Implemented

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //Logic of the game goes here
+let gameSeq = [];
+let userSeq = [];
+let h2 = document.querySelector("h2");
+
+let started = false;
+let level = 0;
+
+document.addEventListener("keypress", function () {
+  if (started == false) {
+    console.log("Game Started!");
+    h2.innerHTML = "Game Started!!!";
+    started = true;
+  }
+});

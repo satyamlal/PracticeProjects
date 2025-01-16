8615c2a87b64b723b5c5a2c211eb3eb2cc76a958
Satyam Lal <satyamlal@users.noreply.github.com>
2025-01-16 21:09:17 +0530

    Debouncing the input fields for 200ms and then trying to hit the backend to calculate the sum

--- BackEnd/Calculator/script.js
+++ BackEnd/Calculator/script.js
@@ -2,22 +2,18 @@
 const calculateBtn = document.querySelector("#sumIt");
 const finalSum = document.querySelector("#finalSum");
 
-// Calculating the sum using fetch
-function calculateSum() {
-  const fnum = document.querySelector("fnum").value;
-  const snum = document.querySelector("snum").value;
+let timeout; //empty variable which will take the counter value
 
-  fetch("https://sum-server.100xdevs.com/sum?a=" + fnum + "&b=" + snum).then(
-    (response) => {
-      response.text().then((ans) => {
-        finalSum.innerHTML = ans;
-      });
-    }
-  );
+function debounceCalculateSum() {
+  clearTimeout(timeout); // clears the old clock value
+  setTimeout(() => {
+    //wait for 200ms and then call the function calculateSum
+    timeout = calculateSum;
+  }, 200);
 }
 
 //Calculating the sum by explicitly making the function async
-async function calculateSum2() {
+async function calculateSum() {
   const fnum = document.querySelector("#fnum");
   const snum = document.querySelector("#snum");
   const response = await fetch(
@@ -26,3 +22,17 @@ async function calculateSum2() {
   const ans = await response.text();
   finalSum.innerHTML = ans;
 }
+
+// Calculating the sum using fetch
+// function calculateSum() {
+//   const fnum = document.querySelector("fnum").value;
+//   const snum = document.querySelector("snum").value;
+
+//   fetch("https://sum-server.100xdevs.com/sum?a=" + fnum + "&b=" + snum).then(
+//     (response) => {
+//       response.text().then((ans) => {
+//         finalSum.innerHTML = ans;
+//       });
+//     }
+//   );
+// }

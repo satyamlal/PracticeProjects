9ad512c5245756eccb13b4fe02c31c1879dc7839
Satyam Lal <satyamlal@users.noreply.github.com>
2025-01-19 00:02:30 +0530

    Started working on basic SQL Project

---
+++ BackEnd/SQLPractice/index.js
@@ -0,0 +1,23 @@
+const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "LearningTheDB@#223",
+  database: "test",
+});
+
+let createRandomUser = () => {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+  };
+};
+
+console.log(createRandomUser());

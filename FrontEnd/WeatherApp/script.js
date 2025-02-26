df0222787041d8b7d5df7d42ecbb7d1c0d57d6d1
Satyam Lal <satyamlal@users.noreply.github.com>
2025-02-26 13:32:02 +0530

    data is being fetched but the API call is not working properly

--- FrontEnd/WeatherApp/script.js
+++ FrontEnd/WeatherApp/script.js
@@ -1,18 +1,25 @@
 const apiKey = "e8a945a7ce1eda1a853f40cb57f9acfc";
 const apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric?q=bangalore";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric?q=";
 
-async function checkWeather() {
-  const response = await fetch(apiURL + `&appid=${apiKey}`);
+const inputBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiURL + city + `&appid=${apiKey}`);
 
   var data = await response.json();
 
   console.log(data);
 
   document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = data.main.temp;
-  document.querySelector(".humidity").innerHTML = data.main.humidity;
-  document.querySelector(".wind").innerHTML = data.wind.speed;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
 }
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(inputBox.value);
+});
 
 checkWeather();

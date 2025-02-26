67854fc4e18d4eb9a0094ec3390ad4bd21cfff8c
Satyam Lal <satyamlal@users.noreply.github.com>
2025-02-26 11:49:15 +0530

    City data is updating on frontend

--- FrontEnd/WeatherApp/script.js
+++ FrontEnd/WeatherApp/script.js
@@ -6,6 +6,13 @@ async function checkWeather() {
   const response = await fetch(apiURL + `&appid=${apiKey}`);
 
   var data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = data.main.temp;
+  document.querySelector(".humidity").innerHTML = data.main.humidity;
+  document.querySelector(".wind").innerHTML = data.wind.speed;
 }
 
 checkWeather();

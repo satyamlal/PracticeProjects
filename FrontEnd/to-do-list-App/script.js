daa66f8d0dd23537f0c17256acbd60e84a32a669
Satyam Lal <satyamlal@users.noreply.github.com>
2025-02-27 19:16:18 +0530

    Lists can be added now in the app

--- FrontEnd/to-do-list-App/script.js
+++ FrontEnd/to-do-list-App/script.js
@@ -1,2 +1,25 @@
 const inputBox = document.querySelector("#input-box");
-const listContainer = document.querySelector("#list")
+const listContainer = document.querySelector("#list-container");
+const errorMessage = document.querySelector(".error");
+const errorPara = document.querySelector("p");
+
+function addTask() {
+  if (inputBox.value.trim() === "") {
+    errorMessage.style.display = "flex";
+    errorPara.textContent = "You must write something!!!";
+  } else {
+    // Hide error message if it's visible
+    errorMessage.style.display = "none";
+
+    // Create a new list item for each task
+    const li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+
+    let span = document.createElement("span");
+    span.textContent = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+}
+
